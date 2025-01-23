a5f77f97efe8d17cb5b9f324e8ca3748c914f741
Arvitoyoga <93527203+Arvitoyoga@users.noreply.github.com>
2025-01-23 21:34:59 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -174,7 +174,7 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(3000).promise().done(function(){
+			$("p:nth-child("+i+")").fadeOut('slow').delay(1500).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(5000);
 			if(i==50){
